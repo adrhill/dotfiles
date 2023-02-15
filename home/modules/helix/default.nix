58f80111e9ebf4ccd6292df7ecba2e7f2a4f1fc8
adrhill <adrian.hill@mailbox.org>
2023-02-15 14:05:17 +0100

    Add Julia LSP to Helix

--- home/modules/helix/default.nix
+++ home/modules/helix/default.nix
@@ -21,6 +21,23 @@ in
         language-server = { command = "rnix-lsp"; };
         formatter = { command = "nix fmt"; args = [ "-f -" ]; }; # read from standard input
       }
+      {
+        name = "julia";
+        scope = "source.julia";
+        file-types = [ "jl" ];
+        roots = [ "Manifest.toml" "Project.toml" ];
+        language-server = {
+          command = "julia";
+          timeout = 60;
+          args = [
+            "--startup-file=no"
+            "--history-file=no"
+            "--quiet"
+            "-e"
+            "using LanguageServer; runserver()"
+          ];
+        };
+      }
     ];
   };
 }
