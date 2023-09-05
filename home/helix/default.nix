3e81d70c5987a7ace439c338e30e08fd2a7e3275
adrhill <adrian.hill@mailbox.org>
2023-09-05 13:10:23 +0200

    Update Helix config

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -25,56 +25,58 @@ in
         };
       };
     };
-    languages = [
-      {
-        name = "nix";
-        auto-format = true;
-        indent = { tab-width = 2; unit = " "; };
-        language-server = { command = "rnix-lsp"; };
-        formatter = { command = "nix fmt"; args = [ "-f -" ]; }; # read from standard input
-      } # end Nix
-      {
-        name = "julia";
-        scope = "source.julia";
-        file-types = [ "jl" ];
-        roots = [ "Manifest.toml" "Project.toml" ];
-        language-server = {
-          command = "julia";
-          timeout = 60;
-          args = [
-            "--startup-file=no"
-            "--history-file=no"
-            "--quiet"
-            "-e"
-            "using LanguageServer; runserver()"
-          ];
-        };
-      } # end Julia
-      {
-        name = "rust";
-        scope = "source.rust";
-        file-types = [ "rs" ];
-        roots = [ "Cargo.toml" "Cargo.lock" ];
-        auto-format = true;
-        comment-token = "//";
-        language-server = {
-          command = "rust-analyzer";
-        };
-        config = {
-          check.command = "clippy";
-          cachePriming.enable = false;
-          diagnostics.experimental.enable = true;
-          inlayHints = {
-            bindingModeHints.enable = false;
-            closingBraceHints.minLines = 10;
-            closureReturnTypeHints.enable = "with_block";
-            discriminantHints.enable = "fieldless";
-            lifetimeElisionHints.enable = "skip_trivial";
-            typeHints.hideClosureInitialization = false;
+    languages = {
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          indent = { tab-width = 2; unit = " "; };
+          language-server = { command = "rnix-lsp"; };
+          formatter = { command = "nix fmt"; args = [ "-f -" ]; }; # read from standard input
+        } # end Nix
+        {
+          name = "julia";
+          scope = "source.julia";
+          file-types = [ "jl" ];
+          roots = [ "Manifest.toml" "Project.toml" ];
+          language-server = {
+            command = "julia";
+            timeout = 60;
+            args = [
+              "--startup-file=no"
+              "--history-file=no"
+              "--quiet"
+              "-e"
+              "using LanguageServer; runserver()"
+            ];
           };
-        };
-      } # end Rust
-    ]; # end languages
+        } # end Julia
+        {
+          name = "rust";
+          scope = "source.rust";
+          file-types = [ "rs" ];
+          roots = [ "Cargo.toml" "Cargo.lock" ];
+          auto-format = true;
+          comment-token = "//";
+          language-server = {
+            command = "rust-analyzer";
+          };
+          config = {
+            check.command = "clippy";
+            cachePriming.enable = false;
+            diagnostics.experimental.enable = true;
+            inlayHints = {
+              bindingModeHints.enable = false;
+              closingBraceHints.minLines = 10;
+              closureReturnTypeHints.enable = "with_block";
+              discriminantHints.enable = "fieldless";
+              lifetimeElisionHints.enable = "skip_trivial";
+              typeHints.hideClosureInitialization = false;
+            };
+          };
+        } # end Rust
+      ]; # end language
+    }; # end languages
   };
 }
 
